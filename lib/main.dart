e5aea435e10a09cb7659926eb11a7af5dd1b5db4
YSK1923 <yavuzselkaya2@gmail.com>
2026-05-05 01:04:45 +0300

    Refactor main.dart to use RootScreen and StudentSetupScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,67 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'services/ai_service.dart';
 import 'services/student_model_service.dart';
 import 'screens/home_screen.dart';
+import 'screens/student_setup_screen.dart';
 
 void main() {
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => StudentModelService()),
-        ChangeNotifierProvider(create: (_) => AIService()),
-      ],
-      child: const AIEgitimApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class AIEgitimApp extends StatelessWidget {
-  const AIEgitimApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AI Öğretmenim',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6C63FF),
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => StudentModelService()),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: const RootScreen(),
       ),
-      home: const HomeScreen(),
     );
+  }
+}
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({super.key});
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    await context.read<StudentModelService>().loadStudent();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final service = context.watch<StudentModelService>();
+
+    if (isLoading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+
+    if (!service.hasStudent) {
+      return const StudentSetupScreen();
+    }
+
+    return const HomeScreen();
   }
 }
